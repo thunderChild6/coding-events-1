82793b5d50efa3175875d5b7dd54b3b6a91e2d94
SarahF <sarahfrerichs@gmail.com>
2019-12-18 22:58:34 -0600

    12.2.3 Add a Model Property

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -3,9 +3,11 @@ package org.launchcode.codingevents.models;
 public class Event {
 
     private String name;
+    private String description;
 
-    public Event(String name) {
+    public Event(String name, String description) {
         this.name = name;
+        this.description = description;
     }
 
     public String getName() {
@@ -16,8 +18,16 @@ public class Event {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
-        return name;
+        return name + ": " + description;
     }
 }
